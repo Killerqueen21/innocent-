86e1585360b02f448dbf1a80be12c420a9e49d65
Killer queen <sharmakhushi0559@gmail.com>
2026-03-08 19:03:34 +0530

    Update autoreload.py

--- scripts/autoreload.py
+++ scripts/autoreload.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2022-present kastaid
-# https://github.com/kastaid/getter
+# Copyright (C) 2022-present Killerqueen21 
+# https://github.com/Killerqueen21/Innocent
 # AGPL-3.0 License
 
 import signal
